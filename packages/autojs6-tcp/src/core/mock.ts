3544a94243912ce951147204f62a7b4b97ba6675
戴海洋 <daihaiyang@xiaohongshu.net>
2025-05-30 17:36:25 +0800

    ✨ feat: command

--- packages/autojs6-tcp/src/core/mock.ts
+++ packages/autojs6-tcp/src/core/mock.ts
@@ -7,3 +7,27 @@ const run = {
   type: "command",
   data: { id: 2, name: "script.js", script: "console.show()", command: "run" },
 }
+
+const runProject = {
+  type: "bytes_command",
+  md5: "c45c4504c0d91d8167533b92a1356eb9",
+  data: {
+    id: "/Users/daihaiyang/Desktop/my-code/test/test-autojs",
+    name: "/Users/daihaiyang/Desktop/my-code/test/test-autojs",
+    deletedFiles: [],
+    override: false,
+    command: "run_project",
+  },
+}
+
+const saveProject = {
+  type: "bytes_command",
+  md5: "5d94581d7c86a92add81ddf2da65e9b5",
+  data: {
+    id: "/Users/daihaiyang/Desktop/my-code/test/test-autojs",
+    name: "/Users/daihaiyang/Desktop/my-code/test/test-autojs",
+    deletedFiles: [],
+    override: true,
+    command: "save_project",
+  },
+}
